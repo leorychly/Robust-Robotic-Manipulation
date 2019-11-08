e59445e236df78f6c8faa9f50530d66dd4e38cd6
rychly <rychly@fortiss.org>
2019-11-08 15:39:49 +0100

    changed BaseAgent structure

--- src/agents/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Collects all defined and implemented agent models."""
-
-from src.agents.base_agent import BaseAgent
-from src.agents.dqn_agent import DQNAgent
-
-__all__ = ["BaseAgent", "DQNAgent"]
--- src/agents/base_agent.py
+++ src/agents/base_agent.py
@@ -15,23 +15,70 @@ class BaseAgent(metaclass=ABCMeta):
     self.executer = executer or BaseExecuter()
 
   def sense(self, state):
+    """
+    Add variability to the state based on the observation module.
+
+    :param state:
+      The environment's current state.
+
+    :return obs:
+      The state observation.
+    """
     obs  = self.observer(state)
     return obs
 
-  def plan(self, state):
-    obs = self.sense(state)
-    action = self._plan(obs)
-    control = self.act(action)
-    return action
+  @abstractmethod
+  def plan(self, obs):
+    """
+    Choose a next action given a observation of the environment.
 
-  def plan_random(self, obs=None):
-    action = self.action_space.sample()
-    return action
+    :param obs:
+      A observation of the environment at the current time step.
+      The observation is an altered stated produced by the observer.
+
+    :return action:
+      Next action to be executed by the agent.
+    """
+    raise NotImplementedError
 
   def act(self, action):
-    control = self.executer(action)
-    return control
+    """
+    Add variability to the action based on the executer module.
 
-  def _plan(self, obs):
-    action = self.plan_random()
+    :param action:
+      The action chosen by the agent.
+
+    :return control_action:
+      The actual action that is executed.
+    """
+    control_action = self.executer(action)
+    return control_action
+
+  def run(self, state):
+    """
+    Run the agent sense-plan-act pipeline.
+
+    :param state:
+      The current state of the environment.
+
+    :return control_action:
+      Return the action that is next executed.
+    """
+    obs = self.sense(state)
+    action = self.plan(obs)
+    control_action = self.act(action)
+    return control_action
+
+
+class RandomAgent(BaseAgent):
+  """This agent implements the Sense-Plan-Act architecture."""
+
+  def __init__(self,
+               action_space,
+               observer=None,
+               executer=None):
+    super(RandomAgent, self).__init__(action_space=action_space)
+
+  def plan(self, obs=None):
+    action = self.action_space.sample()
     return action
