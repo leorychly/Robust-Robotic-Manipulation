457ba36ef5f390847705b56b6692d6f52fa73269
rychly <rychly@fortiss.org>
2019-11-12 17:06:50 +0100

    added json config read

---
+++ src/utils.py
@@ -0,0 +1,19 @@
+import os
+from pathlib2 import Path
+from os.path import isfile, isdir, join
+import json
+
+def read_config(config_path):
+  config_path = str(config_path.absolute())
+  if isfile(config_path):
+    with open(config_path) as jfile:
+        data = json.load(jfile)
+        return data
+  else:
+    print("Error: This is not a file!")
+
+
+if __name__ == '__main__':
+    p = Path(os.getcwd()).parent / "experiments/configs/exp01.json"
+    data = read_config(p)
+    print(data)
