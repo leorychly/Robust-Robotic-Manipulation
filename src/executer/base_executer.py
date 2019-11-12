d560aa81bab7007bff6d64494e9e1555a531bfc2
rychly <rychly@fortiss.org>
2019-11-12 17:43:22 +0100

    update

--- src/executer/base_executer.py
+++ src/executer/base_executer.py
@@ -1,10 +1,38 @@
 from abc import ABCMeta, abstractmethod
+import numpy as np
 
 class BaseExecuter(metaclass=ABCMeta):
   """This agent implements the Sense-Plan-Act architecture."""
 
-  def __init__(self, max_action=None):
-    self.max_action=max_action
+  def __init__(self, action_limits):
+    """
+    Initialize the action limits of the executer.
+
+    :param action_limits: nd.array
+      The min and max values the actions can have in the form of:
+      [[a1_min, a1_max], [a2_min, a2_max], [..], ..]
+    """
+    if action_limits.ndim == 1:
+      action_limits = action_limits.reshape(1, -1)
+    self.action_limits=action_limits
 
   def __call__(self, action):
-    return action
+    """
+    Modify the agent's action.
+
+    :param action: nd.array
+      The agent's action which can be modified.
+
+    :return control action: nd.array
+      The action that will actually be executed.
+    """
+    if not isinstance(action, np.ndarray):
+      action = np.array(action)
+    action = action.astype(np.float)
+    if  hasattr(self, "_modify"):
+      action = self._modify(action)
+    clipped_action = np.clip(action,
+                             a_min=self.action_limits[:, 0],
+                             a_max=self.action_limits[:, 1])
+    return clipped_action
+
