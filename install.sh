b186fa2b319ac663b3f5e10a8825a49724ad99f5
rychly <rychly@fortiss.org>
2019-11-14 11:26:43 +0100

    pybulletgym now in install script

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 virtualenv -p python3.6 ./venv
 source ./venv/bin/activate && pip install -r utils/requirements.txt
+
+mkdir ./environments/
+cd ./environments/
+git clone https://github.com/benelot/pybullet-gym.git
+cd pybullet-gym
+pip install -e .
